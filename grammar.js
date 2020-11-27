0ab57ada414bf9a39967b8b7eefe4c7ae089d0f2
Brian Ryall <brianryall@fastmail.com>
2020-11-26 21:44:40 -0500

    fix whitespace

--- grammar.js
+++ grammar.js
@@ -1,60 +1,59 @@
 module.exports = grammar({
-  name: 'tasks',
+    name: 'tasks',
 
-  rules: {
-    file: $ => repeat($.item),
+    rules: {
+        file: $ => repeat($.item),
 
-    item: $ =>
-      seq(
-        '+',
-        optional($.status),
-        optional(field('id', $.reference)),
-        optional($.tag_list),
-        ':',
-        field('title', $.text),
-        optional($.body),
-      ),
+        item: $ => seq(
+            '+',
+            optional($.status),
+            optional(field('id', $.reference)),
+            optional($.tag_list),
+            optional($.repeat)
+            ':',
+            field('title', $.text),
+            optional($.body),
+        ),
 
-    tag_list: $ => repeat1($.tag),
+        tag_list: $ => repeat1($.tag),
 
-    tag: $ => seq('#', $._word),
+        tag: $ => seq('#', $._word),
 
-    body: $ => seq(
-      '{',
-      repeat(choice(
-        $.text,
-        $.item,
-        $.reference,
-        $.attachement
-      )),
-      '}'
-    ),
+        body: $ => seq(
+            '{',
+            repeat(choice(
+                $.text,
+                $.item,
+                $.reference,
+                $.attachement
+            )),
+            '}'
+        ),
 
-    attachement: $ => seq("@",
-      /(\w|[\/.])+/),
+        attachement: $ => seq("@", /(\w|[\/.])+/),
 
-    reference: $ => seq('&', $._word),
+        reference: $ => seq('&', $._word),
 
-    status: $ => choice(
-      'TODO',
-      'DOING',
-      'DONE',
-      seq('SCHEDULED(', $.date, ')'),
-      seq('DUE(', $.date, ')'),
-      seq('DO(', $.date, ')')
-    ),
+        status: $ => choice(
+            'TODO',
+            'DOING',
+            'DONE',
+            seq('SCHEDULED(', $.date, ')'),
+            seq('DUE(', $.date, ')'),
+            seq('DO(', $.date, ')')
+        ),
 
-    date: $ => seq(
-      field("year", choice(/\d{4}/, '-')),
-      '-',
-      field('month', /\d{2}/),
-      '-',
-      optional(field('day', /\d{2}/))
-    ),
+        date: $ => seq(
+            field("year", choice(/\d{4}/, '-')),
+            '-',
+            field('month', /\d{2}/),
+            '-',
+            optional(field('day', /\d{2}/))
+        ),
 
-    _word: $ => /[a-zA-Z_]\w*/,
+        _word: $ => /[a-zA-Z_]\w*/,
 
-    // Everything appart from special chars in the grammar
-    text: $ => /[^{}+&@]+/,
-  },
+        // Everything appart from special chars in the grammar
+        text: $ => /[^{}+&@]+/,
+    },
 });
